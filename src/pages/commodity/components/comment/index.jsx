483032287d7dc34d72d9fff3f0e5c8e507fd8932
negar <negar.moradi@digikala.com>
2023-06-16 04:06:10 +0330

    update comments

--- src/pages/commodity/components/comment/index.jsx
+++ src/pages/commodity/components/comment/index.jsx
@@ -1,23 +1,49 @@
-import like from '../../../../assets/icons/like.svg';
-import dislike from '../../../../assets/icons/dislike.svg';
+import like from "../../../../assets/icons/like.svg";
+import dislike from "../../../../assets/icons/dislike.svg";
+import UseApi from "../../../../hooks/api";
 
-const Comment = ({comment}) => {
-    return (
-        <div className="row comment-row">
-            <div className="col-xxl-8">
-                <p className="comment py-3">This was awfulllllllllllllllllll!!!!</p>
-                <div className="comment-details d-flex">
-                    <p>{comment.date}</p>
-                    <ul>
-                        <li>{comment.username}</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="col-xxl-4 d-flex justify-content-end align-items-end">
-                <p className="comment-rate d-flex align-items-center">Is this comment helpful? <span>1 </span> <img src={like} alt="like icon"/> <span>1 </span> <img src={dislike} alt="dislike icon"/></p>
-            </div>
+const Comment = ({ comment, getCommentFn }) => {
+  const { apiCall } = UseApi();
+
+  const onVoteSuccess = (res) => {
+    getCommentFn();
+  };
+
+  const voteApiCall = (query) => {
+    apiCall({
+      url: `http://localhost:5432/api/comments/vote/${comment.id}`,
+      query,
+      method: "post",
+      sucessCallback: onVoteSuccess,
+    });
+  };
+
+  const onLike = () => {
+    voteApiCall({ vote: 1 });
+  };
+  const onDislike = () => {
+    voteApiCall({ vote: -1 });
+  };
+  return (
+    <div className="row comment-row">
+      <div className="col-xxl-8">
+        <p className="comment py-3">{comment.text}</p>
+        <div className="comment-details d-flex">
+          <p>{comment.date}</p>
+          <ul>
+            <li>{comment.username}</li>
+          </ul>
         </div>
-    )
-}
+      </div>
+      <div className="col-xxl-4 d-flex justify-content-end align-items-end">
+        <p className="comment-rate d-flex align-items-center">
+          Is this comment helpful? <span>{comment.numberOfLikes}</span>{" "}
+          <img src={like} alt="like icon" onClick={onLike} /> <span>{comment.numberOfDisLikes}</span>{" "}
+          <img src={dislike} alt="dislike icon" onClick={onDislike} />
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Comment;
--- src/pages/commodity/index.jsx
+++ src/pages/commodity/index.jsx
@@ -1,91 +1,139 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { userSelectors } from '../../store/user/selector';
-import UseApi from '../../hooks/api';
-import './commodity.css';
+import UseApi from "../../hooks/api";
+import "./commodity.css";
 import CommodityDetail from "./components/commodityDetail";
 import Comment from "./components/comment";
 import SuggestedCommodity from "./components/suggestedCommodity";
 const Commodity = () => {
-    const { id } = useParams();
-    const user = useSelector(userSelectors.user)
-    const [commodity, setCommodity] = useState([]);
-    const [suggestedCommodities, setSuggestedCommodities] = useState([]);
-    const { apiCall } = UseApi();
-    const onSuccessCommodities = (res) => {
-        setCommodity(res.data.data);
-    }
+  const { id } = useParams();
+  const [commodity, setCommodity] = useState([]);
+  const [suggestedCommodities, setSuggestedCommodities] = useState([]);
+  const [commentInput, setCommentInput] = useState("");
+  const { apiCall } = UseApi();
+  const onSuccessCommodities = (res) => {
+    setCommodity(res.data.data);
+  };
 
-    const onSuccessSuggestedCommodities = (res) => {
-        setSuggestedCommodities(res.data.data);
-    }
+  const onSuccessSuggestedCommodities = (res) => {
+    setSuggestedCommodities(res.data.data);
+  };
 
-    const getCommodityApiCall = () => {
-        const query = {
-            "username": user.username,
-            "password": user.password
-        }
+  const getCommodityApiCall = () => {
+    const query = {};
 
-        apiCall({ url: `http://localhost:5432/api/commodities/${id}`, query, method: 'get', sucessCallback: onSuccessCommodities })
-    }
+    apiCall({
+      url: `http://localhost:5432/api/commodities/${id}`,
+      query,
+      method: "get",
+      sucessCallback: onSuccessCommodities,
+    });
+  };
 
-    const getSuggestedCommoditiesApiCall = () => {
-        const query = {
-            "username": user.username,
-            "password": user.password
-        }
+  const getSuggestedCommoditiesApiCall = () => {
+    const query = {};
 
-        apiCall({ url: `http://localhost:5432/api/commodities/suggestedCommodities/${id}`, query, method: 'get', sucessCallback: onSuccessSuggestedCommodities })
-    }
+    apiCall({
+      url: `http://localhost:5432/api/commodities/suggestedCommodities/${id}`,
+      query,
+      method: "get",
+      sucessCallback: onSuccessSuggestedCommodities,
+    });
+  };
 
-    useEffect(() => {
-        getCommodityApiCall();
-        getSuggestedCommoditiesApiCall();
-      }, [id])
-    
-    return (
+  useEffect(() => {
+    getCommodityApiCall();
+    getSuggestedCommoditiesApiCall();
+  }, [id]);
+
+  const onSaveCommentSuccessfully = (res) => {
+    getCommodityApiCall();
+  };
+
+  const onSaveComment = () => {
+    apiCall({
+      url: `http://localhost:5432/api/commodities/comment/${commodity.id}`,
+      query: { comment: commentInput },
+      method: "post",
+      sucessCallback: onSaveCommentSuccessfully,
+    });
+  };
+
+  Array.prototype.getLength = function() {
+    return this.reduce((sum, elt) => 
+      sum + (elt.length ? elt.getLength() : 1), 0);
+  };
+
+  return (
     <div className="commodity">
-
-    <div className="pt-5 main">
-        <CommodityDetail commodity={commodity}/>
+      <div className="pt-5 main">
+        <CommodityDetail commodity={commodity} />
         <div className="comments ml-5">
-            { commodity?.comments && Object.values(commodity?.comments)[0] && (Object.values(commodity?.comments)[0]).map((comment, index) => {
-                return (
-                    <>
-                        {index === 0 && <p className="comment-title m-0">Comments <span className="comment-counts">({Object.values(commodity?.comments)[0].length})</span></p>}
-                        <Comment comment={comment} key={index}/>
-                    </>
-                )
-            })}
-            <div className="row comment-row">
-                <div className="d-xxl-flex pt-4 align-items-end">
-                    <div className="opinion w-100">
-                        <p>Submit your opinion</p>
-                        <input className="w-100 border-0" type="text"/>
-                    </div>
-                    <button className="submit-button py-3 px-4 border-0 ">Post</button>
-                </div>
+          {commodity?.comments &&
+            Object.values(commodity?.comments) &&
+            Object.values(commodity?.comments).map((comment, index) => (
+              <>
+                {comment.map((item) => (
+                  <>
+                    {index === 0 && (
+                      <p className="comment-title m-0">
+                        Comments{" "}
+                        <span className="comment-counts">
+                          ({Object.values(commodity?.comments).getLength()})
+                        </span>
+                      </p>
+                    )}
+                    <Comment
+                      comment={item}
+                      key={index}
+                      getCommentFn={getCommodityApiCall}
+                    />
+                  </>
+                ))}
+              </>
+            ))}
+          <div className="row comment-row">
+            <div className="d-xxl-flex pt-4 align-items-end">
+              <div className="opinion w-100">
+                <p>Submit your opinion</p>
+                <input
+                  className="w-100 border-0"
+                  type="text"
+                  value={commentInput}
+                  onChange={(e) => {
+                    setCommentInput(e.target.value);
+                  }}
+                />
+              </div>
+              <button
+                className="submit-button py-3 px-4 border-0"
+                onClick={onSaveComment}
+              >
+                Post
+              </button>
             </div>
-       </div>
-       <div className="more-items mx-5 mt-3 mb-5">
-        {suggestedCommodities && 
+          </div>
+        </div>
+        <div className="more-items mx-5 mt-3 mb-5">
+          {suggestedCommodities && (
             <>
-                <p className="py-5">You also might like...</p>
-                <div className="row g-5 more-item">
-                    {suggestedCommodities.map((suggestedCommodity, index) => {
-                        return(
-                            <SuggestedCommodity key={index} suggestedCommodity={suggestedCommodity}/>
-                        )
-                    })}
-                </div>
+              <p className="py-5">You also might like...</p>
+              <div className="row g-5 more-item">
+                {suggestedCommodities.map((suggestedCommodity, index) => {
+                  return (
+                    <SuggestedCommodity
+                      key={index}
+                      suggestedCommodity={suggestedCommodity}
+                    />
+                  );
+                })}
+              </div>
             </>
-        }   
-       </div>
+          )}
+        </div>
+      </div>
     </div>
-
-</div>
-    )
-}
+  );
+};
 
 export default Commodity;
