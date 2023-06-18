59cc45df47ae66e8aaa483aa5657eb9aff5ee801
negar <negar.moradi@digikala.com>
2023-06-18 23:00:11 +0330

    add pagination

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,43 +1,62 @@
-
-import HomeHeader from './components/Header';
-import HomeSetting from './components/Setting';
-import './home.css';
-import Product from '../../components/Product';
-import React, { useEffect, useState } from 'react';
-import UseApi from '../../hooks/api';
+import HomeHeader from "./components/Header";
+import HomeSetting from "./components/Setting";
+import "./home.css";
+import Product from "../../components/Product";
+import React, { useEffect, useState } from "react";
+import UseApi from "../../hooks/api";
+import Pagination from "../../components/pagination";
 
 const Home = () => {
+  const [commodities, setCommodities] = useState([]);
+  const [showedItems, setShowedItems] = useState([]);
+  const { apiCall } = UseApi();
+  const onSuccess = (res) => {
+    setCommodities(res.data.data);
+  };
+  const getCommoditiesApiCall = () => {
+    const query = {};
 
-    const [commodities, setCommodities] = useState([]);
-    const { apiCall } = UseApi();
-    const onSuccess = (res) => {
-        setCommodities(res.data.data);
-    }
-    const getCommoditiesApiCall = () => {
-        const query = {}
+    apiCall({
+      url: "http://localhost:5432/api/commodities",
+      query,
+      method: "get",
+      sucessCallback: onSuccess,
+    });
+  };
 
-        apiCall({ url: "http://localhost:5432/api/commodities", query, method: 'get', sucessCallback: onSuccess })
-    }
+  useEffect(() => {
+    getCommoditiesApiCall();
+  }, []);
 
-    useEffect(() => {
-        getCommoditiesApiCall();
-    }, [])
-    return (
+  useEffect(() => {
+    console.log("showedItems");
+    console.log(showedItems);
+  }, [showedItems]);
 
-        <>
-            <HomeHeader onSearch={setCommodities}/>
-            <div className="px-5 pt-5 home">
-                <HomeSetting onSort={setCommodities}/>
-                <div className='items'>
-                    <div className='row'>
-                        {commodities?.map((product, index) => {
-                            return <Product key={index} product={product}/>
-                        })}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    console.log("commodities");
+    console.log(commodities);
+  }, [commodities]);
+  return (
+    <>
+      <HomeHeader onSearch={setCommodities} />
+      <div className="px-5 pt-5 home">
+        <HomeSetting onSort={setCommodities} />
+        <div className="items">
+          <div className="row">
+            {showedItems.map((product, index) => {
+              return <Product key={index} product={product} />;
+            })}
+          </div>
+          { commodities.length && <Pagination
+            itemsPerPage={10}
+            items={commodities}
+            setCurrentItems={setShowedItems}
+          />}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
